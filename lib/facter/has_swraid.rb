85fa1d8c82d6a1d92ebfab666563434f716db4b4
Samuli Seppänen <samuli.seppanen@gmail.com>
2018-05-10 15:40:50 +0300

    Fix rubocop warnings
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

--- lib/facter/has_swraid.rb
+++ lib/facter/has_swraid.rb
@@ -1,22 +1,18 @@
 Facter.add('has_swraid') do
-
-  # This fact implementation will only work on Linux
-  confine :kernel => 'Linux'
-
+  confine kernel: 'Linux'
   setcode do
-    dir='/dev/md'
-    retval=false
-
+    dir = '/dev/md'
+    retval = false
     # The /dev/md directory does not exist if Linux software RAID is not
     # configured.
     if File.directory?(dir)
-      Dir.foreach(dir) { |entry|
+      Dir.foreach(dir) do |entry|
         # Check if any of the entries are block devices. If yes, we assume that
         # software RAID is configured on the system.
         if File.stat("#{dir}/#{entry}").blockdev?
-          retval=true
+          retval = true
         end
-      }
+      end
     end
     retval
   end
--- lib/facter/hwraid_type.rb
+++ lib/facter/hwraid_type.rb
@@ -1,17 +1,11 @@
 Facter.add('hwraid_type') do
-
-  # This implementation will not work on anything except Linux
-  confine :kernel => 'Linux'
-
+  confine kernel: 'Linux'
   setcode do
     retval = nil
-
-    File.open("/proc/bus/pci/devices", "r") do |devices|
+    File.open('/proc/bus/pci/devices', 'r') do |devices|
       while (device = devices.gets)
-
         # Check if the line has some RAID controller on it
-        match = device.match(/(3w-9xxx|megaraid_sas|aacraid|3w-sas)/)
-
+        match = device.match(%r{/(3w-9xxx|megaraid_sas|aacraid|3w-sas)/})
         if match
           # Set the return value to the matched string (e.g. "3w-9xxx")
           retval = match[0]
